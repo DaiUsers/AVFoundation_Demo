724a3bf7a09afdb1b8729e95610339d121a80825
wheng <2466059429@qq.com>
2019-04-08 10:14:24 +0800

    Initial Commit

---
+++ AVFoundation_Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AVFoundation_Demo
+//
+//  Created by wheng on 2019/4/8.
+//  Copyright © 2019年 wheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
